8488d60a857248eba81c9d368ddc2abcc319df84
curious725 <curious725@gmail.com>
2018-01-17 12:23:44 +0200

    add class Topic

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics')
+    starter = models.ForeignKey(User, related_name='topics')
