d7ab8b0092cd3e19963c06015d507c206a8e7de8
curious725 <curious725@gmail.com>
2018-01-20 13:25:19 +0200

    return in http response boards names

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import pdb
+
+from .models import Board
+
 
 def home(request):
-    return HttpResponse('Hello World!')
+    boards_list = Board.objects.all()
+    boards_names = list()
+
+    for board in boards_list:
+        boards_names.append(board.name)
+
+    html_response = '<br>'.join(boards_names)
+
+    return HttpResponse(html_response)
