25b1f676780af4ac4c5b471bdf5228cd342bcd60
curious725 <curious725@gmail.com>
2018-01-21 18:00:59 +0200

    rewrite board_topics using get_object_or_404

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Board
 
@@ -11,10 +11,7 @@ def home(request):
 
 
 def board_topics(request, pk):
-    try:
-        board = Board.objects.get(pk=pk)
-    except Board.DoesNotExist:
-        raise Http404
+    board = get_object_or_404(Board, pk=pk)
     return render(
         request, 'boards/topics.html', {'board': board}
     )
