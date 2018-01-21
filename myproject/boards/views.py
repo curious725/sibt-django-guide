1736fc4b56ebfdabaa7d77905fe6272adf850d3e
curious725 <curious725@gmail.com>
2018-01-21 12:37:01 +0200

    rewrite home view to render template

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import pdb
-
 from .models import Board
 
 
 def home(request):
-    boards_list = Board.objects.all()
-    boards_names = list()
-
-    for board in boards_list:
-        boards_names.append(board.name)
-
-    html_response = '<br>'.join(boards_names)
-
-    return HttpResponse(html_response)
+    boards = Board.objects.all()
+    return render(
+        request, 'boards/home.html', {'boards': boards}
+    )
