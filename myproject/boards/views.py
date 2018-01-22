36b57dc05f04337486865b4ea499ab25b69caf2d
curious725 <curious725@gmail.com>
2018-01-22 14:12:37 +0200

    add new_topic view

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -15,3 +15,10 @@ def board_topics(request, pk):
     return render(
         request, 'boards/topics.html', {'board': board}
     )
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(
+        request, 'boards/new_topic.html', {'board': board}
+    )
