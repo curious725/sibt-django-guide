45effbc1d91cf14259faa1923c70954da685bb0b
curious725 <curious725@gmail.com>
2018-01-23 11:45:55 +0200

    naive implementation of a view

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
-from .models import Board
+from django.contrib.auth.models import User
+from .models import Board, Topic, Post
 
 
 def home(request):
@@ -19,6 +20,29 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+
+    if request.method == "POST":
+        subject = request.POST["subject"]
+        message = request.POST["message"]
+
+        user = User.objects.first()     # TODO: get currently logged in user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        # import pdb;pdb.set_trace()
+
+        # TODO: redirect to the created topic page
+        return redirect('boards:board_topics', pk=board.pk)
+
     return render(
         request, 'boards/new_topic.html', {'board': board}
     )
