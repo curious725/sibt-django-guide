7b4e257e744b65eab2e5f8091e1c1cafa72ff407
curious725 <curious725@gmail.com>
2018-01-24 11:47:35 +0200

    add help_text in NewTopicForm

--- myproject/boards/forms.py
+++ myproject/boards/forms.py
@@ -1,13 +1,18 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 from .models import Topic
 
 
 class NewTopicForm(forms.ModelForm):
     message = forms.CharField(
         widget=forms.Textarea(),
-        max_length=4000
+        max_length=4000,
+        help_text='The max length of the text is 4000.'
     )
 
     class Meta:
         model = Topic
         fields = ['subject', 'message']
+        help_texts = {
+            'subject': _('The max length of the text is 255.'),
+        }
