060ac386272879b21661b79ddcf2d4686aa21caf
curious725 <curious725@gmail.com>
2018-01-25 13:28:00 +0200

    fix urls for accounts

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'accounts'
-urlspatterns = [
+urlpatterns = [
     # ex: /accounts/signup/
     url('signup/$', views.signup, name='signup'),
 ]
