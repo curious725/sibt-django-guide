00d6f5f7115091ee45cc4c503339e15141cd6e57
curious725 <curious725@gmail.com>
2018-01-26 14:41:41 +0200

    implement login in signup view

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,10 +1,19 @@
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def signup(request):
-    form = UserCreationForm()
-    # import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect(
+                'home'
+            )
+    else:
+        form = UserCreationForm()
     return render(
         request, 'accounts/signup.html', {'form': form}
     )
