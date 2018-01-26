41b0a82b122c2c10c22a2be3c94597441028a269
curious725 <curious725@gmail.com>
2018-01-26 13:28:48 +0200

    use in signup UserCreationForm

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    form = UserCreationForm()
+    # import pdb; pdb.set_trace()
+    return render(
+        request, 'accounts/signup.html', {'form': form}
+    )
