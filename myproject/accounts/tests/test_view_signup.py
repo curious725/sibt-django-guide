45aa3ff09b2b8aa00a73ac8abfdce547dd9c1472
curious725 <curious725@gmail.com>
2018-01-28 13:50:05 +0200

    add test to verify that SignUpForm has only 5 inputs to avoid someone will add some extra fields to it and this change could pass unnotices

--- myproject/accounts/tests/test_view_signup.py
+++ myproject/accounts/tests/test_view_signup.py
@@ -27,6 +27,16 @@ class SignUpTests(TestCase):
             form, SignUpForm
         )
 
+    def test_form_input(self):
+        """
+        The view must contain five inputs:
+        csrf, username, email, password1, password2.
+        """
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
 
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
