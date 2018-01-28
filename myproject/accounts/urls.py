9716052152b300603c07a356174b98c5847565fc
curious725 <curious725@gmail.com>
2018-01-28 15:30:27 +0200

    add url for logout

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,4 +7,6 @@ app_name = 'accounts'
 urlpatterns = [
     # ex: /accounts/signup/
     url('signup/$', views.signup, name='signup'),
+    # ex: /accounts/logout/
+    url('logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
