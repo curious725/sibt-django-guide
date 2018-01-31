36ff090a3828cdeb1140f829ff7a05c7bc8b6283
curious725 <curious725@gmail.com>
2018-01-31 20:08:28 +0200

    add accounts/urls.py

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -1,5 +1,17 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.contrib.auth.views import(
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+
 
 from . import views
 
@@ -8,11 +20,40 @@ urlpatterns = [
     # ex: /accounts/signup/
     url('signup/$', views.signup, name='signup'),
     # ex: /accounts/logout/
-    url('logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url('logout/$', LogoutView.as_view(), name='logout'),
     # ex: /accounts/login/
     url(
         'login/$',
-        auth_views.LoginView.as_view(template_name='accounts/login.html'),
+        LoginView.as_view(template_name='accounts/login.html'),
         name='login'
     ),
+    url(r'^password_reset/$',
+        PasswordResetView.as_view(
+            template_name='accounts/password_reset.html',
+            success_url=reverse_lazy('accounts:password_reset_done'),
+            email_template_name='accounts/password_reset_email.html',
+            subject_template_name='accounts/password_reset_subject.txt'
+        ),
+        name='password_reset'),
+    url(r'^password_reset/done/$',
+        PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_done.html',
+        ),
+        name='password_reset_done'),
+
+    url(r'^password_reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/'
+        '(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_confirm.html',
+            success_url=reverse_lazy('accounts:password_reset_complete'),
+        ),
+
+        name='password_reset_confirm'),
+
+    url(r'^password_reset/complete/$',
+        PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'
+        ),
+        name='password_reset_complete'),
+
 ]
